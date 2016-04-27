836d0fd219fec09d76b202bd3083557a38df5999
david.garcia <david.garcia.olivares@rwth-aachen.de>
2016-04-27 18:51:09 +0200

    Falta ajustar el fujo de la linea al dibujar

--- 23/src/Code/Expert/ExtraCreditApp.java
+++ 23/src/Code/Expert/ExtraCreditApp.java
@@ -1,5 +1,79 @@
 package Code.Expert;
 
-public class ExtraCreditApp {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics; 
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter; 
+import javax.swing.JComponent;
+import javax.swing.JFrame; 
+
+/**
+ *
+ * @author David
+ */
+public class ExtraCreditApp extends JFrame {
+
+    private final class Widget extends JComponent {
+
+        Point startPoint = new Point(0, 0);
+        Point endPoint = new Point(0, 0); 
+        
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.red); 
+            g.drawLine(startPoint.x , startPoint.y, startPoint.y - endPoint.x, startPoint.y); 
+        }
+
+        public void setStart(Point startPoint) {
+            this.startPoint = startPoint;
+        }
+        
+        public Point getStart(){
+            return this.startPoint;
+        }
+        
+        public void setEnd(Point endPoint) {
+            this.endPoint = endPoint;
+            repaint();
+        }
+    }
+
+    public ExtraCreditApp() {
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        Widget myWidget = new Widget();
+        myWidget.setPreferredSize(new Dimension(600, 600));
+        this.add(myWidget, BorderLayout.CENTER);
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(java.awt.event.MouseEvent e) { 
+                myWidget.setEnd(e.getPoint());
+            }
+        });
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                myWidget.setStart(e.getPoint());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                //myWidget.setStart(new Point(0, 0));
+            }
+        });
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String arg[]) {
+        ExtraCreditApp app = new ExtraCreditApp();
+    }
 
 }
