85546b0c90675658e54773750b3d450a9f603d76
Tania G Velasco <tania.garcia@rwth-aachen.de>
2016-04-27 19:37:13 +0200

    la línea pintándose de acuerdo al movimiento vertical

--- 23/src/Code/Expert/ExtraCreditApp.java
+++ 23/src/Code/Expert/ExtraCreditApp.java
@@ -16,8 +16,13 @@ import javax.swing.JFrame;
  * @author David
  */
 public class ExtraCreditApp extends JFrame {
+    
+    int endYcoord;
 
     private final class Widget extends JComponent {
+        
+        int height = 600;
+        int widht = 600;
 
         Point startPoint = new Point(0, 0);
         Point endPoint = new Point(0, 0); 
@@ -25,17 +30,18 @@ public class ExtraCreditApp extends JFrame {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.setColor(Color.red); 
-            g.drawLine(startPoint.x , startPoint.y, startPoint.y - endPoint.x, startPoint.y); 
+            g.setColor(Color.black); 
+            g.drawLine(startPoint.x , startPoint.y, height - endPoint.y, startPoint.y); 
+            System.out.println(endPoint.y);
         }
 
-        public void setStart(Point startPoint) {
-            this.startPoint = startPoint;
+        public void setStart() {
+            this.endPoint.y= height;
+            this.startPoint.x = 0;
+            this.startPoint.y= this.endPoint.x= 280;
+            repaint();
         }
         
-        public Point getStart(){
-            return this.startPoint;
-        }
         
         public void setEnd(Point endPoint) {
             this.endPoint = endPoint;
@@ -47,6 +53,7 @@ public class ExtraCreditApp extends JFrame {
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         Widget myWidget = new Widget();
         myWidget.setPreferredSize(new Dimension(600, 600));
+        myWidget.setStart();
         this.add(myWidget, BorderLayout.CENTER);
 
         this.addMouseMotionListener(new MouseMotionAdapter() {
@@ -59,12 +66,12 @@ public class ExtraCreditApp extends JFrame {
         this.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                myWidget.setStart(e.getPoint());
+            
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                //myWidget.setStart(new Point(0, 0));
+            
             }
         });
 
@@ -77,3 +84,5 @@ public class ExtraCreditApp extends JFrame {
     }
 
 }
+
+
