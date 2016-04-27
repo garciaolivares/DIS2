880042ebed6fd217d9802dd3ead10a9d38336c19
tan13 <tania.garcia@rwth-aachen.de>
2016-04-27 20:52:14 +0200

    Update WindowSystem.java
    comentada

--- 23/src/Code/WindowSystem.java
+++ 23/src/Code/WindowSystem.java
@@ -7,27 +7,38 @@ public class WindowSystem extends GraphicsEventSystem{
 	double width, height;
 	float StartX, StartY, EndX, EndY;
 	
+	//initialization 
     public WindowSystem(int i, int j) {
-        super(i, j);
-        width = (double)i;
+    	
+        super(i, j); // call parent class to draw our "desktop" with size ixj
+        
+        // define width and height of our desktop with values i and j
+        width = (double)i; 
         height = (double)j;
         
     }
          
     @Override
     protected void handlePaint() {
-    	
+    	// draw a single line from (0.2, 0.3) to (0.8, 0.7) with resolution independet drawline method
     	this.drawLine(0.2f, 0.3f, 0.8f, 0.7f);
     }
     
+    //resolution independent drawline 
     public void drawLine(float StartX, float StartY, float EndX, float EndY){
     	
-    	StartX= (float) (StartX * width);
+    	/*
+    	*  Take floating values of coordinates and multiply by current height and widht of our desktop
+    	*  new values will match desktop size
+    	*/
+    	StartX= (float) (StartX * width); 
     	StartY= (float) (StartY * height);
     	EndX= (float)(EndX * width);
     	EndY= (float)(EndY * height);	
-    	super.setColor(Color.black);
-    	super.drawLine(Math.round(StartX), Math.round(StartY), Math.round(EndX), Math.round(EndY));
+    	
+    	super.setColor(Color.black); //set color of line
+    	//rount values to int to use drawline method in GraphicsEventSystem library
+    	super.drawLine(Math.round(StartX), Math.round(StartY), Math.round(EndX), Math.round(EndY)); 
     }
     
 }
