e89380cd987ac264544baf5ad27e84a84f912cfd
tan13 <tania.garcia@rwth-aachen.de>
2016-04-27 20:39:58 +0200

    Update MyApp.java
    comentada

--- 23/src/Code/MyApp.java
+++ 23/src/Code/MyApp.java
@@ -3,6 +3,7 @@ package Code;
 public class MyApp {
 	
 	public static void main(String args[]){
+		//create object WindowSystem to create and draw rectangle (desktop)
 		WindowSystem desktop = new WindowSystem(800,600);
 	}
 
