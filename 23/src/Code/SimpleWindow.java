ce21ea640912cc70e0a38ab41437eb56f82d74fd
david.garcia <david.garcia.olivares@rwth-aachen.de>
2016-05-01 15:57:33 +0200

    add comments

--- 23/src/Code/SimpleWindow.java
+++ 23/src/Code/SimpleWindow.java
@@ -1,20 +1,22 @@
 package Code;
 public class SimpleWindow {
 
-    private int locationX = 0;
-    private int locationY = 0;
-    private int locationZ = 0;
+    private int locationX = 0; //attibute for position in x-axis
+    private int locationY = 0; //attibute for position in y-axis
+    private int locationZ = 0; //attibute for depth in z-axis
 
     public SimpleWindow(int x, int y, int z) {
         locationX = x;
         locationY = y;
         locationZ = z;
     }
-
-    int getX() {
+    
+    //getters and setters for x, y, and z position
+    
+    int getX() { 
         return locationX;
     }
-
+	
     void setX(int x) {
         this.locationX = x;
     }
