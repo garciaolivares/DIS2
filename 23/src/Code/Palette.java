803db5c21463aa481eb463b663c130712abf123d
Tania G Velasco <tania.garcia@rwth-aachen.de>
2016-05-04 13:00:49 +0200

    actualización en Palette y WindowManager

--- 23/src/Code/Palette.java
+++ 23/src/Code/Palette.java
@@ -2,19 +2,29 @@ package Code;
 import java.awt.Color;
 
 
-public class Palette { 
-    private Color blue = new Color(43,79,137);
-    private Color lightBlue = new Color(55,110,158);
-    private Color lightGray = new Color(213,205,198); 
+public class Palette {
+    
     private Color black = new Color(0,0,0);
     private Color white = new Color(255,255,255);
+    private Color gray = new Color(128,128,128);
+    private Color blue = new Color(43,79,137);
+    private Color red = new Color(255,102,102);
+    private Color yellow = new Color(249,212,137);
+    private Color lightBlue = new Color(0,128,255);
+    private Color lightGray = new Color(228,228,228);
+    private Color darkGray = new Color(112,112,112);
     
     
     
-    public Color blue(){  return blue; }
-    public Color lightGray(){  return lightGray; }
-    public Color lightBlue(){  return lightBlue; }
     public Color black(){  return black; }
     public Color white(){  return white; }
-    
+    public Color gray(){  return gray; }
+    public Color blue(){  return blue; }
+    public Color red(){ return red; }
+    public Color yellow(){ return yellow; }
+    public Color lightGray(){  return lightGray; }
+    public Color darkGray(){  return darkGray; }
+    public Color lightBlue(){  return lightBlue; }
+  
+
 }
